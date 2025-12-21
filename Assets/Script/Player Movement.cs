266204f8e3aecd4356541c04b25828dca5aaaca9
Raka Ahmad Abrisam <rakaahmadabrisam@gmail.com>
2025-12-21 23:29:34 +0700

    Player Movement

---
+++ Assets/Script/Player Movement.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private Vector2 moveInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        ReadInput();
+        //UpdateAnimation();
+        FlipSprite();
+    }
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = moveInput * moveSpeed;
+        Debug.Log(rb.linearVelocity);
+    }
+
+
+    private void ReadInput()
+    {
+        if (Keyboard.current == null)
+        {
+            moveInput = Vector2.zero;
+            return;
+        }
+
+        float x =
+            (Keyboard.current.dKey.isPressed ? 1 : 0) -
+            (Keyboard.current.aKey.isPressed ? 1 : 0);
+        
+        float y =
+            (Keyboard.current.wKey.isPressed ? 1 : 0) -
+            (Keyboard.current.sKey.isPressed ? 1 : 0);
+
+        moveInput = new Vector2(x, y).normalized;
+        Debug.Log($"Move Input: {moveInput}");
+    }
+
+    private void UpdateAnimation()
+    {
+        animator.SetFloat("MoveX", moveInput.x);
+        animator.SetFloat("MoveY", moveInput.y);
+        animator.SetFloat("Speed", moveInput.sqrMagnitude);
+    }
+
+    private void FlipSprite()
+    {
+        if (moveInput.x < 0)
+            spriteRenderer.flipX = true;
+        else if (moveInput.x > 0)
+            spriteRenderer.flipX = false;
+    }
+}
