f64921b2cb2e9185c4d6807af05df9beeb8bffe5
Raka Ahmad Abrisam <rakaahmadabrisam@gmail.com>
2026-01-05 02:02:39 +0700

    Animasi Anjing

--- Assets/Script/Dog Bark Controller.cs
+++ Assets/Script/Dog Bark Controller.cs
@@ -1,35 +1,36 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
+
 public class DogBarkController : MonoBehaviour
 {
     [Header("Bark Settings")]
     public float barkDuration = 1.5f;
-
     [Header("References")]
     public NPCWaypointMovement movement;
-    private Animator animator;
-
     private bool isBarking = false;
-
     public void ActiveBark()
     {
         if (isBarking) return;
 
-        // emit suara anjing
-        NoiseSystem.Emit(new NoiseInfo(NoiseType.DogBark, transform.position, 10f));
+        NoiseSystem.Emit(new NoiseInfo(
+            NoiseType.DogBark,
+            transform.position,
+            10f
+        ));
 
         StartCoroutine(BarkRoutine());
     }
 
-
     private IEnumerator BarkRoutine()
     {
         isBarking = true;
-        if(animator != null)
-            animator.SetBool("Bark", true);
-        movement.StopMovement(false);
-        yield return new WaitForSeconds(barkDuration);
+
+        // 🔴 BERHENTI SAAT BARK
         movement.StopMovement(true);
+        yield return new WaitForSeconds(barkDuration);
+        // 🟢 LANJUT JALAN SETELAH BARK
+        movement.StopMovement(false);
+
         isBarking = false;
     }
 }
